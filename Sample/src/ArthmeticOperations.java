48d98d15d6ead2ea0f76e978a2e7b7d3cfcb3fab
Hironobu-okubo <ee171068oh@gmail.com>
2022-04-27 02:27:20 +0900

    add SampleProject

---
+++ Sample/src/ArthmeticOperations.java
@@ -0,0 +1,15 @@
+
+public class ArthmeticOperations {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        int num1 = 100;
+        int num2 = 10;
+
+        int result1 = num1 + num2;
+        System.out.println("num1 + num2 = " + result1);
+
+
+    }
+
+}
---
+++ Sample/src/Sample01.java
@@ -0,0 +1,12 @@
+
+public class Sample01 {
+
+    public static void main(String[] args) {
+        int number = 1;
+        String str1 = "1";
+        System.out.println(number);
+        System.out.println(str1);
+
+    }
+
+}
