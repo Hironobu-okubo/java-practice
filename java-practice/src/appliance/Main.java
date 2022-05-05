363ba04c9ed1e37eeacf91f77ab1f2fa81aec757
Hironobu-okubo <ee171068oh@gmail.com>
2022-05-06 00:33:12 +0900

    add Main class

---
+++ java-practice/src/appliance/Main.java
@@ -0,0 +1,11 @@
+package appliance;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Appliance fridge01 = new Appliance("冷蔵庫","ブランド",10000);
+        fridge01.getInfo();
+
+    }
+
+}
