632e60b6e40ce7faba53a4985aa891748900519a
Hironobu-okubo <ee171068oh@gmail.com>
2022-05-06 01:23:07 +0900

    update Main class

--- java-practice/src/appliance/Main.java
+++ java-practice/src/appliance/Main.java
@@ -1,13 +1,42 @@
 package appliance;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Appliance> inputList = new ArrayList<Appliance>();
         Fridge fridge01 = new Fridge("冷蔵庫","ブランド",10000,1000);
-        fridge01.getInfo();
+        inputList.add(fridge01);
         Tv tv01 = new Tv("Tv01","ブランド",10000,45,"無");
-        tv01.getInfo();
+        inputList.add(tv01);
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("最低価格を入力してください。");
+        int inputMin = scanner.nextInt();
+        System.out.println("最高価格を入力してください。");
+        int inputMax = scanner.nextInt();
+        scanner.close();
+
+        ArrayList<Appliance> outputList = checkPrice(inputMin,inputMax,inputList);
+        for(Appliance app:outputList){
+            app.getInfo();
+        }
+
+
+
+    }
+
+    public static ArrayList<Appliance> checkPrice(int inputMin, int inputMax, ArrayList<Appliance> inputList){
+        ArrayList<Appliance> outputList = new ArrayList<Appliance>();
+        for(Appliance app:inputList){
+            if(app.getPrice() <= inputMax && app.getPrice() >= inputMin){
+                outputList.add(app);
+            }
+        }
+        return outputList;
     }
 
 }
